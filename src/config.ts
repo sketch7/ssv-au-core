5d2ad21ba8aa57abc1ced86f4ffd97c2e4c403bf
Clayton Lautier <claylaut@hotmail.com>
2017-10-14 15:16:46 +0200

    fix(web pack): wrap `globalResources` in `PLATFORM.moduleName`

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,13 @@
-import { FrameworkConfiguration } from "aurelia-framework";
+import { FrameworkConfiguration, PLATFORM } from "aurelia-framework";
+
 import { RouteMapper, routeActiveConfig } from "./routing/index";
 import { CoreConfig } from "./config.model";
 
 export function configure(frameworkConfig: FrameworkConfiguration, config: CoreConfig): Promise<void> {
 	frameworkConfig.singleton(RouteMapper);
 	frameworkConfig.globalResources([
-		"./routing/route-active/route-active.attribute",
-		"./routing/route-href/route-href.attribute"
+		PLATFORM.moduleName("./routing/route-active/route-active.attribute"),
+		PLATFORM.moduleName("./routing/route-href/route-href.attribute")
 	]);
 
 	Object.assign(routeActiveConfig, config.routeActive);
